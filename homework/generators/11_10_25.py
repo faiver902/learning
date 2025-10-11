a2d8e18415e0a44d27003fb2ec3091d99c228d08
faiver90 <faiver90@gmail.com>
2025-10-11 17:29:33 +0300

    'дз от 11/10/25'

---
+++ homework/generators/11_10_25.py
@@ -0,0 +1,44 @@
+
+
+def sliding_window(seq, num):
+    """
+    Напиши генератор sliding_window(seq, n), который будет возвращать последовательность "окон" длиной n по списку seq.
+    # [(1,2,3), (2,3,4), (3,4,5)]
+    """
+    length = len(seq)
+    seq_id = 0
+    while seq_id + num <= length:
+        yield seq[seq_id : seq_id + num]
+        seq_id += 1
+
+
+print(list(sliding_window([1, 2, 3, 4, 5], 3)))
+
+print("\n" + "#" * 80 + "\n")
+
+
+def cycle_words(words):
+    """
+    Напиши генератор cycle_words(words), который будет бесконечно выдавать элементы списка words по кругу.
+    gen = cycle_words(["red", "green", "blue"])
+    [next(gen) for _ in range(7)]
+    # ['red', 'green', 'blue', 'red', 'green', 'blue', 'red']
+
+    по итогу реализация подсмотрел у cycle
+    """
+    # cycle_words_gen = cycle(words)
+    # while True:
+    #     yield next(cycle_words_gen)
+    saved = []
+    for word in words:
+        saved.append(word)
+
+    while saved:
+        for word in saved:
+            yield word
+
+
+gen = cycle_words(["red", "green", "blue"])
+list(next(gen) for _ in range(5))
+
+print("\n" + "#" * 80 + "\n")
